72c78cfed769b407eb81381103902e9f90a0a2bc
Jojordanme <135418765+Jojordanme@users.noreply.github.com>
2024-05-26 12:56:41 +0000

    Error page = Unfinished page

--- functions.js
+++ functions.js
@@ -1,11 +1,17 @@
 const lvl1btn = document.getElementById("beginner")
 const lvl2btn = document.getElementById("intermidiate")
+const lvl3btn = document.getElementById("advanced")
+
 lvl1btn.addEventListener("click",() => {
   console.log("yesyesyes")
     window.location.replace("somePages/beginner/pathway.html")
 })
 lvl2btn.addEventListener("click",() => {
   console.log("yesyesyes")
-    window.location.replace("somePages/intermidiate/pathway.html")
+    window.location.replace("errorpage.html")
+})
+lvl3btn.addEventListener("click",() => {
+  console.log("yesyesyes")
+    window.location.replace("errorpage.html")
 })
 

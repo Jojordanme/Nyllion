47182fdaac470d0228026d87f3dae60b338a1a82
Jojordanme <135418765+Jojordanme@users.noreply.github.com>
2024-06-27 12:04:41 +0000

    Essays and MORE!

--- somePages/beginner/Scripts/Levels/level4.js
+++ somePages/beginner/Scripts/Levels/level4.js
@@ -12,103 +12,43 @@ function englishNumbersWord(num){
 
 const quizData = [
 {
-    question: "Bahasa Inggrisnya 'Nanas'",
-    a: "Apple",
-  b: "Orange",
-    c: "Banana",
-  d: "Pineapple",
-  correct:"d",
-  explanation:"'Nanas' dalam bahasa Inggris merupakan 'Pineapple'.",
+    question: "Bahasa Inggrisnya 'Apel'",
+    
+  correct:["apple"],
+  explanation:"'Apel' dalam bahasa Inggris merupakan 'Apple'.",
 },
   {
     question: "Bahasa Inggrisnya 'Tas'",
-      a: "Pencil",
-      b: "Back",
-      c: "Book",
-      d: "Bag",
-      correct: "d",
-    explanation:"'Tas' dalam bahasa Inggris merupakan 'Bag'",
+      
+      correct: ["bag","backpack"],
+    explanation:"'Tas' dalam bahasa Inggris adalah 'Bag'",
   },
   {
       question: `Bahasa Inggrisnya "Air"`,
-      a: "Air",
-      b: "Water",
-      c: "Cyan",
-      d: "Yellow",
-      correct: "b",
+      
+      correct: ["water"],
     explanation:"'Air' dalam bahasa Inggris adalah 'Water'",
   },
   {
       question: "Bahasa Indonesianya 'Pencil'",
-      a: "Pensil",
-      b: "Buku",
-      c: "Tas",
-      d: "Tujuh",
-      correct: "a",
+      
+      correct:["pensil"],
     explanation:"'Pencil' dalam bahasa indonesia adalah 'Pensil'",
   },
   {
-      question: "Bahasa Inggrisnya 'Pagi'",
-      a: "Morning",
-      b: "Noon",
-      c: "Afternoon",
-      d: "Night",
-      correct: "a",
-    explanation:"Bahasa Inggris dari 'Pagi' adalah 'Morning'",
-  },
-  {
-      question: "Bahasa Inggrisnya 'Iya' ",
-      a: "No",
-      b: "Never",
-      c: "Yes",
-      d: "Yet",
-      correct: "c",
-    explanation:"Bahasa Inggris dari 'iya' adalah 'yes'",
-  },
-  {
-      question: "Devon __ decent at math",
-      a: "Is",
-      b: "Pencil",
-      c: "Are",
-      d: "Were",
-      correct: "a",
-    explanation:"Kata 'is' digunakan untuk 1 orang saja, Sehingga kata 'Are' dan 'Were' untuk 2 atau lebih orang",
-  },
-   {
-     question: "Bahasa Inggrisnya 'Siang'",
-       a: "Evening",
-       b: "Afternoon",
-       c: "Night",
-       d: "Noon",
-       correct: "d",
-     explanation:"'Evening' arti setelah sore. 'Afternoon' arti dalam waktu sore. 'Night'arti malam. 'Noon' arti siang ",
+      question: "Bahasa Inggrisnya 'Malam'",
+      
+      correct: ["night"],
+    explanation:"Bahasa Inggris dari 'Malam' adalah 'Night'",
   },
 
-  {
-      question: "Bahasa Inggrisnya 'Kenapa?'",
-      a: "Why?",
-      b: "Who?",
-      c: "Where?",
-      d: "How?",
-      correct: "a",
-    explanation : "Kata 'Kenapa' artinya 'Why'",
-  },
-  {
-      question: "They ___ winning!",
-      a: "is",
-      b: "are",
-      c: "was",
-      d: "Semua jawaban salah",
-      correct: "b",
-
-    explanation:"Kata 'Are' digunakan untuk 2 atau lebih orang. 'Is' untuk 1 orang.",
-  },
 ];
 const quizData2 = []
 let canActive = true
 const agj = document.getElementById("yourgoodness")
 const count = document.getElementById("counter")
 const questiontext = document.getElementById("question")
+const esay = document.getElementById("essay")
 const opt1 = document.getElementById("a_text")
 const opt2 = document.getElementById("b_text")
 const opt3 = document.getElementById("c_text")
@@ -146,12 +86,12 @@ function setaquestionanumberthingy(idx){
     quizData[idx].correct = "a"
   } else if (random == 2) {
     quizData[idx].a = numberseng[Math.floor(Math.random() * 31)]
-
+    
     quizData[idx].b = word2
     if (quizData[idx].a == quizData[idx].b){
       quizData[idx].b = numberseng[Math.floor(Math.random() * 31)]
     }
-
+    
     quizData[idx].c = numberseng[Math.floor(Math.random() * 31)]
     if (quizData[idx].c == quizData[idx].b){
       quizData[idx].c = numberseng[Math.floor(Math.random() * 31)]
@@ -171,7 +111,7 @@ function setaquestionanumberthingy(idx){
     if (quizData[idx].b == quizData[idx].c){
       quizData[idx].b = numberseng[Math.floor(Math.random() * 31)]
     }
-
+    
     quizData[idx].d = numberseng[Math.floor(Math.random() * 31)]
     if (quizData[idx].d == quizData[idx].c){
       quizData[idx].d = numberseng[Math.floor(Math.random() * 31)]
@@ -179,7 +119,7 @@ function setaquestionanumberthingy(idx){
     quizData[idx].correct = "c"
   } else {
     quizData[idx].a = numberseng[Math.floor(Math.random() * 31)]
-
+    
     quizData[idx].b = numberseng[Math.floor(Math.random() * 31)]
     quizData[idx].c = numberseng[Math.floor(Math.random() * 31)]
     quizData[idx].d = word2
@@ -194,18 +134,17 @@ function setaquestionanumberthingy(idx){
     }
     quizData[idx].correct = "d"
   }
-
+  
 }
 
 // 2,3
-setaquestionanumberthingy(3)
 
 
 
 function loadQuiz(){
   deselectAnswers()
   if (questiontext){questiontext.setAttribute("style","color:white")}
-
+  
   const currentQuize = quizData[currentQuiz]
   questiontext.innerText = currentQuize.question
   count.innerHTML = "Question " + (currentQuiz + 1) + "/" + quizData.length
@@ -214,14 +153,32 @@ function loadQuiz(){
     })
   if (currentQuiz == quizData.length - 1) {
     count.style.color = "red"
-
+    
   } else {
     count.style.color = "white"
   }
-  opt1.innerText = currentQuize.a
-  opt2.innerText = currentQuize.b
-  opt3.innerText = currentQuize.c
-  opt4.innerText = currentQuize.d
+  esay.value = ''
+  if (currentQuize.a){
+    esay.classList.add("hide")
+
+    opt1.classList.remove("hide")
+    opt2.classList.remove("hide")
+    opt3.classList.remove("hide")
+    opt4.classList.remove("hide")
+    opt1.innerText = currentQuize.a
+    opt2.innerText = currentQuize.b
+    opt3.innerText = currentQuize.c
+    opt4.innerText = currentQuize.d
+  } else {
+    esay.classList.remove("hide")
+
+    opt1.classList.add("hide")
+    opt2.classList.add("hide")
+    opt3.classList.add("hide")
+    opt4.classList.add("hide")
+
+  }
+ 
   explanation.innerHTML = ""
     submit.innerHTML = `<span class="button-pathway-shadow"></span>
       <span class="button-pathway-edge" style="background-color:rgb(0,120,0)!important"></span>
@@ -236,7 +193,7 @@ function getSelected(){
   answers.forEach(answerd => {
     if(answerd.checked){
       answer = answerd.id
-
+      
     }      
   })
   return answer
@@ -251,7 +208,7 @@ let ansuorOpt
 var audio = new Audio("sfx/Click.mp3");
 submit.addEventListener("click",() => {
   if (canActive){
-
+ 
     if (phase == 0){
       const answer = getSelected()
       ansuorOpt = answer
@@ -267,44 +224,71 @@ submit.addEventListener("click",() => {
       questiontext.setAttribute("style","color:rgb(0,255,0)")
       labelOption.setAttribute("style","color:rgb(0,255,0)")
       labelOption.innerText += " ✅"
-
+      
       currentScore++
     } else {
       explanation.innerHTML = `<b>Explanasi: ${quizData[currentQuiz].explanation}</b>`
       labelOption.setAttribute("style","color:rgb(255,0,0)")
       correctOption.setAttribute("style","color:rgb(0,255,0)")
       labelOption.innerText += " ❌"
-
+      
       questiontext.innerHTML = "SALAH!"
     }
-
+    
     console.log(answer)
     submit.innerHTML = `<span class="button-pathway-shadow" style="background-color:rgb(18,81,119)!important"></span>
       <span class="button-pathway-edge" style="background-color:rgb(18,81,119)!important"></span>
       <span class="button-pathway-front text" style="background-color:rgb(29,149,319)!important">
         <b>Next</b>
       </span>`
-
+    
   }else{
-    alert("Pilih salah satu jawaban!")
+    const currentQuize = quizData[currentQuiz]
+    if (!currentQuize.a){
+      phase = 1
+
+      const answerEssay = document.getElementById("essay").value
+      if (currentQuize.correct.includes(answerEssay.toLowerCase())){
+        currentScore++
+        questiontext.innerHTML = "BENAR!"
+        questiontext.setAttribute("style","color:rgb(0,255,0)")
+        
+      } else {
+        questiontext.innerHTML = "SALAH!"
+
+        explanation.innerHTML = `<b>Explanasi: ${quizData[currentQuiz].explanation}</b>`
+
+      }
+      submit.innerHTML = `<span class="button-pathway-shadow" style="background-color:rgb(18,81,119)!important"></span>
+      <span class="button-pathway-edge" style="background-color:rgb(18,81,119)!important"></span>
+      <span class="button-pathway-front text" style="background-color:rgb(29,149,319)!important">
+        <b>Next</b>
+      </span>`
+    } else {
+      alert("Pilih salah satu jawaban!")
+
+    }
   }
     }else{
       const answer = ansuorOpt
       const labelOption = document.getElementById(answer+"_text")
     const correctOption = document.getElementById(quizData[currentQuiz].correct+"_text")
-
+      
       canActive = false
       phase = 0
       currentQuiz++
     if(currentQuiz < quizData.length){
-      labelOption.setAttribute("style","color:white")
-      correctOption.setAttribute("style","color:white")
+      if (quizData[currentQuiz - 1].a){
+        labelOption.setAttribute("style","color:white")
+        correctOption.setAttribute("style","color:white")
+      }
+     
 
       loadQuiz()
     }else{
-
-
-      if (currentScore < 6) {
+      
+      
+      if (currentScore <= Math.round(quizData.length / 2)) {
         agj.innerHTML = "Butuh Latihan Lagi"
         quiz.innerHTML = `<h2>Anda menjawab ${currentScore}/${quizData.length} pertanyaan benar</h2>
       <center><button class="button-pathway-pushable" role="button"  onclick="location.reload()">
@@ -314,7 +298,7 @@ submit.addEventListener("click",() => {
         <b>Retry</b>
       </span>
     </button></center>`
-      } else if (currentScore >= 6 && currentScore < 10){
+      } else if (currentScore >= Math.round(quizData.length / 2) && currentScore != quizData.length){
         agj.innerHTML = "Bagus"
         quiz.innerHTML = `<h2>Anda menjawab ${currentScore}/${quizData.length} pertanyaan benar</h2>
       <center><button class="button-pathway-pushable" role="button"  onclick="window.location.replace('${"pathway.html"}')">
@@ -324,7 +308,7 @@ submit.addEventListener("click",() => {
         <b>Back</b>
       </span>
     </button></center>`
-      } else if (currentScore == 10){
+      } else if (currentScore == quizData.length){
         quiz.innerHTML = `<h2>Anda menjawab ${currentScore}/${quizData.length} pertanyaan benar</h2>
       <center><button class="button-pathway-pushable" role="button"  onclick="location.replace('${"pathway.html"}')">
       <span class="button-pathway-shadow" style="background-color:rgb(120,0,0)!important"></span>
--- somePages/beginner/epic.js
+++ somePages/beginner/epic.js
@@ -1,3 +1,11 @@
+document.addEventListener('visibilitychange', function() {
+    // Check if the page is hidden
+    if (document.hidden) {
+        console.log('User opened another tab');
+    } else {
+        window.location.replace("ended.html")
+    }
+});
 
 function teleport(file){
   console.log("am i winning?")
